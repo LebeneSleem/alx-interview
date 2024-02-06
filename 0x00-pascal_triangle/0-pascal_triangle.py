6a487479a0631228479d86c3c047758172cd09bb
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-06 22:44:18 +0000

    pycodestyle error fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-This program defines a function to generate Pascal's triangle up to the nth row,
+This program defines a function to generate Pascal's
+triangle up to the nth row,
 and then prints the triangle using another function.
 """
+
 
 def pascal_triangle(n):
     """
@@ -14,7 +16,8 @@ def pascal_triangle(n):
     triangle = [[1]]
     for _ in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)] + [1]
+        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i\
+                         in range(len(prev_row) - 1)] + [1]
         triangle.append(new_row)
 
     return triangle
