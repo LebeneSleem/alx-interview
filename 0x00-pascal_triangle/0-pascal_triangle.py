4a74e382808ced4695afd6c0346b4c3c9c4b6795
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-06 22:32:33 +0000

    SOlution to question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This program defines a function to generate Pascal's triangle up to the nth row,
+and then prints the triangle using another function.
+"""
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)] + [1]
+        triangle.append(new_row)
+
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+Repo for ALX interview questions
