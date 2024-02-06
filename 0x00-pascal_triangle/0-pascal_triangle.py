bf67277cd99864bdface35d7383cd51e33b48b76
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-06 22:45:47 +0000

    fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     triangle = [[1]]
     for _ in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i\
+        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i
                          in range(len(prev_row) - 1)] + [1]
         triangle.append(new_row)
 
