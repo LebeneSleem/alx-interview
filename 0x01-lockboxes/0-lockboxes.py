8fac70ee071badaafe4bbbdbb4b910c65d91f67b
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-16 12:41:57 -0800

    testing code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
+"""
+a method that determines if all the boxes can be opened
+"""
+
+
 def canUnlockAll(boxes):
-    num_boxes = len(boxes)
-    visited = [False] * num_boxes
+    # Initialize a set to keep track of opened boxes
+    opened_boxes = set()
+    opened_boxes.add(0)  # Start with the first box unlocked
 
-    def dfs(box):
-        if visited[box]:
-            return
-        visited[box] = True
-        for key in boxes[box]:
-            dfs(key)
+    # Initialize a set to keep track of keys
+    keys = set(boxes[0])
 
-    dfs(0)
-    return all(visited)
+    # Iterate through the keys and boxes until no new keys or boxes are found
+    while keys:
+        # Get a key from the set of keys
+        key = keys.pop()
+
+        # If the key corresponds to a box that hasn't been opened yet
+        if key < len(boxes) and key not in opened_boxes:
+            # Add the box to the set of opened boxes
+            opened_boxes.add(key)
+            # Add any new keys found in the opened box to the set of keys
+            keys.update(boxes[key])
+
+    # If all boxes are opened, return True; otherwise, return False
+    return len(opened_boxes) == len(boxes)
